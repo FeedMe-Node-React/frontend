cd3458b4ad2d4eded2ebd3d5576954430e562e2a
Phil DeLong <philjdelong@gmail.com>
2020-10-06 10:52:02 -0600

    Return formData in body

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -128,7 +128,13 @@ class Feed extends Component {
     this.setState({
       editLoading: true
     });
-    // Set up data (with image!)
+    const formData = new FormData();
+    console.log(postData)
+    formData.append('title', postData.title)
+    formData.append('content', postData.content)
+    formData.append('image', postData.image)
+    formData.append('userId', this.props.userId)
+
     let url = 'http://localhost:8080/feed/post/';
     let method = 'POST'
     if (this.state.editPost) {
@@ -139,14 +145,9 @@ class Feed extends Component {
     fetch(url, {
       method: method,
       headers: {
-        'Content-Type': 'application/json',
         Authorization: 'Bearer ' + this.props.token,
       },
-      body: JSON.stringify({
-        title: postData.title,
-        content: postData.content,
-        userId: this.props.userId
-      })
+      body: formData
     })
       .then(res => {
         if (res.status !== 200 && res.status !== 201) {
@@ -159,6 +160,7 @@ class Feed extends Component {
           _id: resData.post._id,
           title: resData.post.title,
           content: resData.post.content,
+          imageUrl: resData.post.imageUrl,
           creator: resData.post.creator,
           createdAt: resData.post.createdAt
         };
